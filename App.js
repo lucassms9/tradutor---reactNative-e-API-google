03700f64ab8b0e3fbefd073c7380c09d21b43134
LucasMobitap <lucassms9@hotmail.com>
2018-08-12 00:32:54 -0300

    ajustes
    ajustes

--- App.js
+++ App.js
@@ -9,6 +9,18 @@ import {
   TextInput
 } from "react-native";
 
+class UselessTextInput extends Component {
+  render() {
+    return (
+      <TextInput
+        {...this.props} // Inherit any props passed to it; e.g., multiline, numberOfLines below
+        editable={true}
+        maxLength={40}
+      />
+    );
+  }
+}
+
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -17,7 +29,7 @@ export default class App extends Component {
       languageInicio: "pt",
       languageFim: "en",
       text: "",
-      resultado: "12313123"
+      resultado: ""
     };
 
     this.traduzir = this.traduzir.bind(this);
@@ -44,63 +56,51 @@ export default class App extends Component {
   }
 
   render() {
-    return (
-      <View style={styles.container}>
+    return <View style={styles.container}>
         <View style={styles.header}>
           <Text style={styles.text}>Tradutor</Text>
         </View>
+        <View style={{ flex:1, backgroundColor: "#000" }}>
+          <View style={styles.containerOperadores}>
+            <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <Text>IDIOMA</Text>
 
-        <View style={styles.containerOperadores}>
-          <View style={{ alignItems: "center", justifyContent: "center" }}>
-            <Text>IDIOMA</Text>
+              <Picker selectedValue={this.state.languageInicio} style={{ width: 140 }} onValueChange={(itemValue, itemIndex) => this.setState(
+                    { languageInicio: itemValue }
+                  )}>
+                <Picker.Item label="Português" value="pt" />
+                <Picker.Item label="Inglês" value="en" />
+              </Picker>
+            </View>
 
-            <Picker
-              selectedValue={this.state.languageInicio}
-              style={{ width: 140 }}
-              onValueChange={(itemValue, itemIndex) =>
-                this.setState({ languageInicio: itemValue })
-              }
-            >
-              <Picker.Item label="Português" value="pt" />
-              <Picker.Item label="Inglês" value="en" />
-            </Picker>
+            <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <Text>IDIOMA</Text>
+
+              <Picker selectedValue={this.state.languageFim} style={{ width: 140 }} onValueChange={(itemValue, itemIndex) => this.setState(
+                    { languageFim: itemValue }
+                  )}>
+                <Picker.Item label="Inglês" value="en" />
+                <Picker.Item label="Português" value="pt" />
+              </Picker>
+            </View>
           </View>
 
-          <View style={{ alignItems: "center", justifyContent: "center" }}>
-            <Text>IDIOMA</Text>
+          <View style={styles.container2}>
+            <View style={styles.inputTraducao}>
+              <TextInput style={{ height: 80, fontSize: 25 }} placeholder="Texto para traduzir" onChangeText={text => this.setState(
+                    { text }
+                  )} />
+            </View>
 
-            <Picker
-              selectedValue={this.state.languageFim}
-              style={{ width: 140 }}
-              onValueChange={(itemValue, itemIndex) =>
-                this.setState({ languageFim: itemValue })
-              }
-            >
-              <Picker.Item label="Inglês" value="en" />
-              <Picker.Item label="Português" value="pt" />
-            </Picker>
+            <View style={styles.inputResultado}>
+              <Text style={{ fontSize: 25 }}>{this.state.resultado}</Text>
+            </View>
+            <View style={styles.ButtonTra}>
+              <Button title="TRADUZIR" onPress={this.traduzir} />
+            </View>
           </View>
         </View>
-
-        <View style={styles.inputTraducao}>
-          <TextInput
-            style={{ height: 80 }}
-            placeholder="Texto para traduzir"
-            onChangeText={text => this.setState({ text })}
-          />
-        </View>
-
-        <View style={styles.inputResultado}>
-          <TextInput
-            editable={false}
-            style={{ height: 80 }}
-            value={this.state.resultado}
-            onChangeText={resultado => this.setState({ resultado })}
-          />
-        </View>
-        <Button title="TRADUZIR" onPress={this.traduzir} />
-      </View>
-    );
+      </View>;
   }
 }
 
@@ -109,16 +109,27 @@ const styles = StyleSheet.create({
     flex: 1
   },
   containerOperadores: {
+    // flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    marginTop: 30,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+
+    paddingVertical: 15
+  },
+  inputResultado: {
     marginVertical: 15
   },
   inputTraducao: {
-
+    marginVertical: 15
+  },
+  ButtonTra: {
+    marginVertical: 15
   },
   header: {
-    backgroundColor: "#808080",
-
+    backgroundColor: "#696969",
+    // flex: 1,
     justifyContent: "center",
     alignItems: "center"
   },
@@ -126,5 +137,15 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 30,
     color: "#fff"
+  },
+  container2: {
+    // flex: 4,
+    marginVertical: 15,
+    padding: 15,
+    backgroundColor: "#fff",
+    borderRadius: 10
+  },
+  fundo:{
+    flex:1
   }
 });
